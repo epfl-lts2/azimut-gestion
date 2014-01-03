1a69beecd445ea9874c8d4cccb4c004581ca4c62
Maximilien Cuony <maximilien@theglu.org>
2014-01-03 01:59:37 +0100

    More http headers

--- hostnameforwarding/views.py
+++ hostnameforwarding/views.py
@@ -164,6 +164,11 @@ server {
         """
 
             script += """        access_log off;
+        proxy_set_header        X-Real-IP       $remote_addr;
+        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header        Host            $host;
+        proxy_set_header        X-Forwarded-Proto $scheme;
+
         }
         """
 
