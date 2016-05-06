30ff0876d2e7ddbc3d877c8e68639be1ae7b2539
Maximilien Cuony <maximilien@theglu.org>
2016-05-06 16:10:30 +0200

    Disable port forwarding

--- portforwarding/tasks.py
+++ portforwarding/tasks.py
@@ -9,6 +9,8 @@ from django.conf import settings
 def update_portforwarding():
     """Update all server with portforwarding"""
 
+    return
+
     for server in Server.objects.filter(is_vm=False).exclude(ssh_connection_string_from_gestion=None).exclude(ssh_connection_string_from_gestion='').exclude(external_interface='').all():
 
         os.system('ssh ' + server.ssh_connection_string_from_gestion + ' wget ' + settings.GESTION_URL + 'portforwarding/get_script/' + str(server.pk) + '/nat-vz -O /etc/init.d/nat-vz')
