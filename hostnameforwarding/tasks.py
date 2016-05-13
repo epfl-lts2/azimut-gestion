71591e07de1ff61206efdd41517986e2cc182147
Maximilien Cuony <maximilien@theglu.org>
2016-05-13 13:52:44 +0200

    Disable hostname

--- hostnameforwarding/tasks.py
+++ hostnameforwarding/tasks.py
@@ -9,6 +9,8 @@ from django.conf import settings
 def update_hostnameforwarding():
     """Update all server with taskforwarding info"""
 
+    return
+
     for server in Server.objects.filter(is_vm=False).exclude(ngnix_server=None).exclude(ngnix_server__ssh_connection_string_from_gestion=None).exclude(ngnix_server__ssh_connection_string_from_gestion='').all():
 
         os.system('ssh ' + server.ngnix_server.ssh_connection_string_from_gestion + ' wget ' + settings.GESTION_URL + 'hostnameforwarding/get_conf/' + str(server.pk) + '/ngnix.conf -O /etc/nginx/sites-available/ngnix.conf')
